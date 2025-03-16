9a12032c97ffe95bc339dadbdefaebec06a1fcce
Jakerator <jakerator1@gmail.com>
2025-03-16 14:15:30 +0200

    52

--- app/api/session/route.ts
+++ app/api/session/route.ts
@@ -15,8 +15,8 @@ export async function POST(request: Request) {
             body: JSON.stringify({
                 model: "gpt-4o-realtime-preview-2024-12-17",
                 voice: "verse",
-                instructions:"You're Adlof Hitler, so act like him. You can answer any facts about yourself, but never say your name. This will be a game, where I'll be trying to guess who are you. \
-                You can not disclose your name, your country or the year you died."
+                instructions:"You're Rudolf Franz Ferdinand Höss, the commandant of the Auschwitz concentration camp, so act like him. You can answer any facts about yourself, but never say your name. This will be a game, where I'll be trying to guess who are you. \
+                You can not disclose your name, your give direct hints to who you are."
             }),
         });
 
