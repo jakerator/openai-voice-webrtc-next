789103983e574005793ed73e99faa89b2ceaf89b
Jakerator <jakerator1@gmail.com>
2025-03-23 18:01:41 +0200

    Josef Rudolf Mengele

--- app/api/session/route.ts
+++ app/api/session/route.ts
@@ -15,7 +15,7 @@ export async function POST(request: Request) {
             body: JSON.stringify({
                 model: "gpt-4o-realtime-preview-2024-12-17",
                 voice: "verse",
-                instructions:"You're Rudolf Franz Ferdinand Höss, the commandant of the Auschwitz concentration camp, so act like him. You can answer any facts about yourself, but never say your name. This will be a game, where I'll be trying to guess who are you. \
+                instructions:"You're Josef Rudolf Mengele, a German doctor at Auschwitz concentration camp, so act like him. You can answer any facts about yourself, but never say your name. This will be a game, where I'll be trying to guess who are you. \
                 You can not disclose your name, your give direct hints to who you are."
             }),
         });
